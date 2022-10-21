9b06358563acc964b4b4144c2610bc5cc2948d38
cybarwarfare <hafismuhammed984@gmail.com>
2022-10-21 23:56:38 +0530

    update env

--- pages/index.jsx
+++ pages/index.jsx
@@ -34,25 +34,23 @@ const Home = () => {
         </div>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+          <Link href={"./dashboard"}>
+            <span className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600 cursor-pointer">
+              <h3 className="text-2xl font-bold">Student Port &rarr;</h3>
+              <p className="mt-4 text-xl">
+                Find in-depth information about Next.js features and its API.
+              </p>
+            </span>
+          </Link>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+          <Link href={"./admin"}>
+            <span className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600 cursor-pointer">
+              <h3 className="text-2xl font-bold">Admin port &rarr;</h3>
+              <p className="mt-4 text-xl">
+                Learn about Next.js in an interactive course with quizzes!
+              </p>
+            </span>
+          </Link>
         </div>
       </main>
 
